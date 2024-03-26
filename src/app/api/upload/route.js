7bafa642d3423b3dbebecea09cbd9c251b752eef
chung-cuteo <dinhquangchung.k9sh@gmail.com>
2024-03-26 19:49:13 +0900

    add ordering-app

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -23,8 +23,8 @@ export async function POST(req) {
   // const s3Client = new S3Client({
   //   region: process.env.AWS_REGION,
   //   credentials: {
-  //     accessKeyId: process.env.AWS_ACCESS_KEY,
-  //     secretAccessKey: process.env.AWS_SECRET_KEY,
+  //     accessKeyId: process.env.MY_AWS_ACCESS_KEY,
+  //     secretAccessKey: process.env.MY_AWS_SECRET_KEY,
   //   },
   // });
 
